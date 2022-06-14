fd2cb1bc104acd800f3e367f67c369e1606b03d4
francisco-programmer <franciscopalenciaa@gmai.com>
2022-06-14 12:58:24 -0400

    foto

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
+import React from 'react';
 import Navbar from './components/Navbar'
 import './asset/css/App.css';
 import WeatherPanel from './components/WeatherPanel'
 
 
+
 function App() {
+   
   return (
     <div className="App">
       <Navbar />
-     
       <WeatherPanel />
-     
     </div>
   );
 }
+
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import React from "react"
+
 const Navbar = () => {
 return (
     <nav className="navbar- bg-dark text-light nb-5">
@@ -5,10 +7,7 @@ return (
           <h3 className="mx-auto">Cl     
       <a href='https://www.linkedin.com/in/francisco-palencia-704a0823a/'>
       <img src="https://cdn-icons-png.flaticon.com/512/61/61109.png" alt="linkedin" id="linkedin"/>
-      </a>fra 
-      </h3> 
-        
-        
+      </a>fra</h3> 
         </div>
     </nav>
 )
